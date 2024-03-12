a0d4a09de7de4e2bc956ef7c8c28311890b1f8d6
이예원 <81704418+scarletYewon@users.noreply.github.com>
2024-03-13 04:00:12 +0900

    //

--- app/app/src/main/java/com/android/myapplication/JWT/JwtService.kt
+++ app/app/src/main/java/com/android/myapplication/JWT/JwtService.kt
@@ -7,26 +7,7 @@ import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface JwtService {
-//    @GET("/api/test")
-//    suspend fun getData(): String
-//
-//    @POST("/api/path-test/{temp}")
-//    suspend fun pathTest(@Path("temp") temp: String): String
-//
-//    @GET("/api/param-test")
-//    suspend fun paramTest(@Query("temp") temp: String): String
-//
-//    @POST("/api/header-test")
-//    suspend fun headerTest(@Header("token") token:String): String
-//
-//    @POST("/api/body-test")
-//    suspend fun bodyTest(@Body data:MyRequestData): String
-//
-//    @GET("/user")
-//    suspend fun userTest(@Query("id") id: Int): User
-//
-//    @POST("/user")
-//    suspend fun signTest(@Body data:User): User
+
     @POST("/register")
     suspend fun registerTest(@Body data: User):UserDto
 
